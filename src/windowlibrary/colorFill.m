074513c2b5aa18218148ab75dc87da5daab34d3a
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-17 10:48:47 -0800

    AtaraxiaSDK V0.06
    I've added a bridge to the pointers so that the obj-c pointers work with c pointers

--- src/windowlibrary/colorFill.m
+++ src/windowlibrary/colorFill.m
@@ -1,40 +1,38 @@
 #import "colorFill.h"
+#import <Cocoa/Cocoa.h>
 
-NSColor *ATARAXIA_CUSTOM_RED_01()
+// Define the functions here, only once
+void *ATARAXIA_CUSTOM_RED_01()
 {
-    return [NSColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+    return (__bridge void *)[NSColor colorWithSRGBRed:1.0 green:0.0 blue:0.0 alpha:1.0];
 }
 
-NSColor *ATARAXIA_CUSTOM_BLUE_01()
+void *ATARAXIA_CUSTOM_BLUE_01()
 {
-    return [NSColor colorWithRed:0.0 green:0.0 blue:1.0 alpha:1.0];
+    return (__bridge void *)[NSColor colorWithSRGBRed:0.0 green:0.0 blue:1.0 alpha:1.0];
 }
 
-NSColor *ATARAXIA_CUSTOM_GREEN_01()
+void *ATARAXIA_CUSTOM_GREEN_01()
 {
-    return [NSColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0];
+    return (__bridge void *)[NSColor colorWithSRGBRed:0.0 green:1.0 blue:0.0 alpha:1.0];
 }
 
-/*
-NSColor *ATARAXIA_CUSTOM_YELLOW_01()
+void *ATARAXIA_CUSTOM_YELLOW_01()
 {
-
+    return (__bridge void *)[NSColor colorWithSRGBRed:1.0 green:1.0 blue:0.0 alpha:1.0];
 }
 
-NSColor *ATARAXIA_CUSTOM_ORANGE_01()
+void *ATARAXIA_CUSTOM_ORANGE_01()
 {
-
+    return (__bridge void *)[NSColor colorWithSRGBRed:1.0 green:0.647 blue:0.0 alpha:1.0];
 }
 
-NSColor *ATARAXIA_CUSTOM_PURPLE_01()
+void *ATARAXIA_CUSTOM_PURPLE_01()
 {
-
+    return (__bridge void *)[NSColor colorWithSRGBRed:0.5 green:0.0 blue:0.5 alpha:1.0];
 }
 
-NSColor *ATARAXIA_CUSTOM_PINK_01()
+void *ATARAXIA_CUSTOM_PINK_01()
 {
-
+    return (__bridge void *)[NSColor colorWithSRGBRed:1.0 green:0.75 blue:0.8 alpha:1.0];
 }
-
-We will later fill these in
-*/
