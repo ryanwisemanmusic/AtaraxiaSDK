a7911c618d0f5002f5dd598ffceb7ff9c904f41d
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-17 10:47:22 -0800

    AtaraxiaSDK V0.06
    In the quest to fixing the issues with Obj-C working with color, we had to modify these functions to be extern void functions instead

--- src/windowlibrary/colorFill.h
+++ src/windowlibrary/colorFill.h
@@ -1,22 +1,20 @@
+#ifndef COLOR_FILL_H
+#define COLOR_FILL_H
+
 #ifdef __OBJC__
 #import <Cocoa/Cocoa.h>
 #endif
 
-#ifdef __OBJC__
-NSColor *ATARAXIA_CUSTOM_RED_01();
-NSColor *ATARAXIA_CUSTOM_BLUE_01();
-NSColor *ATARAXIA_CUSTOM_GREEN_01();
-NSColor *ATARAXIA_CUSTOM_YELLOW_01();
-NSColor *ATARAXIA_CUSTOM_ORANGE_01();
-NSColor *ATARAXIA_CUSTOM_PURPLE_01();
-NSColor *ATARAXIA_CUSTOM_PINK_01();
-#else
-void *ATARAXIA_CUSTOM_RED_01();
-void *ATARAXIA_CUSTOM_BLUE_01();
-void *ATARAXIA_CUSTOM_GREEN_01();
-void *ATARAXIA_CUSTOM_GREEN_01();
-void *ATARAXIA_CUSTOM_YELLOW_01();
-void *ATARAXIA_CUSTOM_ORANGE_01();
-void *ATARAXIA_CUSTOM_PURPLE_01();
-void *ATARAXIA_CUSTOM_PINK_01();
-#endif
+// Use void * for both Objective-C and C environments
+extern void *ATARAXIA_CUSTOM_RED_01();
+extern void *ATARAXIA_CUSTOM_BLUE_01();
+extern void *ATARAXIA_CUSTOM_GREEN_01();
+extern void *ATARAXIA_CUSTOM_YELLOW_01();
+extern void *ATARAXIA_CUSTOM_ORANGE_01();
+extern void *ATARAXIA_CUSTOM_PURPLE_01();
+extern void *ATARAXIA_CUSTOM_PINK_01();
+
+#endif // COLOR_FILL_H
+
+
+
