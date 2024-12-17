38111bf53b72b955bf06212a8e8abfc36757c572
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-17 12:23:53 -0800

    AtaraxiaSDK V0.06
    Additional comments explaining how this header functions. Nothing else that is groundbreaking to the code working

--- ataraxia_app_c/window/c_translation/window_ObjCTrans.h
+++ ataraxia_app_c/window/c_translation/window_ObjCTrans.h
@@ -1,3 +1,12 @@
+/*This is our Obj-C header that allows the objects under Windows, 
+which is stored under Obj-C code, to be accessed by C code.
+
+Since a lot of these Cocoa objects and not accessible in standard C 
+(for example, NSWindow is very much something that is easier done in
+Obj-C), we must give some if statements that parse when we reference
+Obj-C.
+*/
+
 #ifndef TRANSLATION_HEADER_H
 #define TRANSLATION_HEADER_H
 
@@ -5,7 +14,9 @@
 #import <Cocoa/Cocoa.h>
 #endif
 
-
+/*If we are referencing Obj-C, define these objects. ifdef is the way
+we handle the bridge between the two languages. Since our backend
+is entirely in Obj-C, this is an important bridge*/
 #ifdef __OBJC__
 NSApplication *ataraxiaApplication();
 NSWindow *createWindow(
