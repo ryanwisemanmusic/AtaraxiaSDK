641aa418ec40f9738446f70a6638343a2e686bbc
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-20 00:36:29 -0800

    AtaraxiaSDK V0.08
    For the amount of problems I was getting trying to pass error messages, and the refusal for my project to recognize simple NSLogs, I've decided to be a bit more smart about how I go about constructing this SDK.
    
    I am just keeping it in Obj-C, however, I will be writing a euclidean graphical engine (which is optional) eventually in C, since that will store much of the math needed.
    
    There is still quite a lot of more debugging to finish up with. However, I am able to accurately diagnose things since I am getting prompted with logs, which wasn't happening before.
    
    TL;DR, WE ARE SO FUCKING BACK

---
+++ ataraxia_app_c/window/screenLogic/screen_management.m
@@ -0,0 +1,53 @@
+#import "screen_management.h"
+#import <Cocoa/Cocoa.h>
+
+@implementation ScreenManager
+
++ (NSArray<NSColor *> *)getScreenColors
+{
+    static NSArray<NSColor *> *screenColors = nil;
+    if (!screenColors)
+    {
+        screenColors = @[
+            [NSColor redColor],
+            [NSColor blueColor]
+        ];
+    }
+    return screenColors;
+}
+
++ (void)showIntroTitle
+{
+    NSLog(@"Showing Intro Title Screen");
+}
+
++ (void)showMainScreen1
+{
+    NSLog(@"Showing Main Screen 1");
+}
+
++ (void)handleScreenTransition:(NSWindow *)window
+{
+    static int currentScreen = 0;
+
+    NSArray<void (^)(void)> *screenHandlers = @[
+        ^{ [self showIntroTitle]; },
+        ^{ [self showMainScreen1]; }
+    ];
+
+    NSArray<NSColor *> *screenColors = [self getScreenColors];
+
+    int nextScreen = (currentScreen + 1) % screenColors.count;
+
+    NSLog(@"Transitioning to Screen %d with color %@", nextScreen, 
+    screenColors[nextScreen]);
+
+    screenHandlers[nextScreen]();
+
+    [window setBackgroundColor:screenColors[nextScreen]];
+
+    currentScreen = nextScreen;
+}
+
+@end
+
