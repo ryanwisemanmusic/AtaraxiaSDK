9e57504e79850f434acdbb8bf7c6931ddc74860d
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-20 21:31:57 -0800

    AtaraxiaSDK V0.08
    While I have better logs on everything, and I've tightened down the problem more, we still have this fundamental problem. It's been REAL hard trying to get this program to work. Luckily, my newest set logs are pinging, which means that things are getting called, I just cannot figure out how to break the code correctly

--- src/windowlibrary/delegate/MouseDelegate.m
+++ src/windowlibrary/delegate/MouseDelegate.m
@@ -1,23 +1,27 @@
 #import "MouseDelegate.h"
+#import "windowCWrapper.h"
 
 @implementation MouseDelegate
 
 - (void)mouseDown:(NSEvent *)event {
-    NSLog(
-        @"Mouse clicked at location: %@", 
-        NSStringFromPoint(event.locationInWindow));
-}
+    NSWindow *window = [event window];
 
-- (void)mouseUp:(NSEvent *)event {
-    NSLog(
-        @"Mouse released at location: %@", 
-        NSStringFromPoint(event.locationInWindow));
-}
+    static int colorIndex = 0;
 
-- (void)mouseMoved:(NSEvent *)event {
-    NSLog(
-        @"Mouse moved to location: %@", 
-        NSStringFromPoint(event.locationInWindow));
+    // Get the colors from the array
+    NSArray<NSColor *> *colors = getScreenColors();
+
+    if (colors) {
+        // Access the current color
+        NSColor *currentColor = colors[colorIndex];
+        setWindowBackgroundColor((__bridge void *)(window), (__bridge void *)(currentColor));
+
+        NSLog(@"Window color switched to: %@", currentColor);
+
+        // Increment and loop through the color array
+        colorIndex = (colorIndex + 1) % colors.count;
+    }
 }
 
 @end
+
