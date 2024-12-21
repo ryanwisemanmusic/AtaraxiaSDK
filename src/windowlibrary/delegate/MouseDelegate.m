a1cff38409b21cc9f853c43e9e6c23fcf9735992
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-20 22:08:46 -0800

    Ataraxia V0.08
    I still have yet to fix this annoying ass bug, but I have figured out some of the problems. These error checks have been useful in knowing what the problem is NOT. We just are still facing a lot of problems with the code

--- src/windowlibrary/delegate/MouseDelegate.m
+++ src/windowlibrary/delegate/MouseDelegate.m
@@ -5,23 +5,36 @@
 
 - (void)mouseDown:(NSEvent *)event {
     NSWindow *window = [event window];
-
+    
     static int colorIndex = 0;
 
     // Get the colors from the array
     NSArray<NSColor *> *colors = getScreenColors();
 
     if (colors) {
-        // Access the current color
+        // Log the current color being applied
         NSColor *currentColor = colors[colorIndex];
+        NSLog(@"Attempting to set window background color: %@", currentColor);
+        
+        // Set the window background color
         setWindowBackgroundColor((__bridge void *)(window), (__bridge void *)(currentColor));
 
-        NSLog(@"Window color switched to: %@", currentColor);
-
+        // Ensure the content view is redrawn
+        NSView *contentView = [window contentView];
+        [contentView setNeedsDisplay:YES];
+        NSLog(@"Window content view redrawing initiated.");
+        
         // Increment and loop through the color array
         colorIndex = (colorIndex + 1) % colors.count;
+    } else {
+        NSLog(@"No colors found in getScreenColors!");
     }
 }
 
+
+
+
+
 @end
+
 
