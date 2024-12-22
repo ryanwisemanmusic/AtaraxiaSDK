ac660a125af6ad28dffd8166ef5627710d931aa7
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-21 20:21:24 -0800

    AtaraxiaSDK V0.10
    Now, the log window prints accordingly, meaning my mouse clicks are recognized and supported.

--- src/windowlibrary/delegate/LogDelegate.m
+++ src/windowlibrary/delegate/LogDelegate.m
@@ -2,6 +2,7 @@
 
 @interface LogDelegate : NSObject
 @property (strong, nonatomic) LogViewer *logViewer;
+@property (strong, nonatomic) NSPipe *logPipe;
 + (instancetype)sharedInstance;
 @end
 
@@ -11,9 +12,10 @@
     static LogDelegate *sharedInstance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedInstance = [[LogDelegate alloc] init]; 
+        sharedInstance = [[LogDelegate alloc] init];
         sharedInstance.logViewer = [[LogViewer alloc] init];
         [sharedInstance.logViewer showLogWindow];
+        [sharedInstance setupLogRedirection];
     });
     return sharedInstance;
 }
@@ -22,4 +24,32 @@
     [self.logViewer addLogMessage:message];
 }
 
+- (void)setupLogRedirection {
+    self.logPipe = [NSPipe pipe];
+    NSFileHandle *fileHandle = self.logPipe.fileHandleForReading;
+
+    // Redirect stderr to the pipe
+    dup2(self.logPipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO);
+
+    // Use a weak reference to avoid a retain cycle
+    __weak NSFileHandle *weakFileHandle = fileHandle;
+    dispatch_queue_t logQueue = dispatch_queue_create("com.logdelegate.queue", DISPATCH_QUEUE_SERIAL);
+    fileHandle.readabilityHandler = ^(NSFileHandle *handle) {
+        NSData *data = handle.availableData;
+        if (data.length > 0) {
+            NSString *log = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            
+            // Dispatch to the main queue to update the UI
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self logMessage:log];
+            });
+        } else {
+            // Use the weak reference to break the retain cycle
+            weakFileHandle.readabilityHandler = nil;
+        }
+    };
+}
+
+
 @end
+
