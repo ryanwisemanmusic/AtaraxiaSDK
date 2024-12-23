cb4d6744514155b3a2a74a271ba16ae5b4f1f870
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-23 13:23:59 -0800

    AtaraxiaSDK V0.16.1
    We updated the AppDelegate to support another aspect of the top windowing element, which is a file section that allows us to showcase how capable the SDK is. Since I am getting issues with windowing and global event issues, I should be able to tackle some of the problems I was having here.
    
    This will be the largest project within this project, since we will use this to help fix one of the biggest problems presented in my code. It's exciting that this is happening.
    
    I am just going for general audio support for now, but it would be cool to have a free file converter that works on Mac. I don't want this SDK to be limited to just audio conversion, but common files people use.

--- AtaraxiaSDK/src/windowlibrary/mainmenu/AppDelegate.h
+++ AtaraxiaSDK/src/windowlibrary/mainmenu/AppDelegate.h
@@ -1,10 +1,13 @@
 #import <Cocoa/Cocoa.h>
+#import "FileConverterWindow.h"
 
 @interface AppDelegate: NSObject <NSApplicationDelegate>
 
 @property (strong, nonatomic) NSMenu *appMenu;
 @property (strong, nonatomic) NSMenuItem *quitMenuItem;
+@property (strong, nonatomic) FileConverterWindowController *FileConverterWindowController;
 
 - (void)quitApplication:(id)sender;
+- (IBAction)showFileConverter:(id)sender;
 
 @end
--- AtaraxiaSDK/src/windowlibrary/mainmenu/AppDelegate.m
+++ AtaraxiaSDK/src/windowlibrary/mainmenu/AppDelegate.m
@@ -5,34 +5,56 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
-        [self setupAppMenu];
+        [self setupAppMenu]; // Set up the app menu when the app starts
     }
     return self;
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification 
-{
-
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Any additional setup you need after the application launches
 }
 
 - (void)setupAppMenu {
+    // Create the main menu
     NSMenu *mainMenu = [[NSMenu alloc] initWithTitle:@"Main Menu"];
 
+    // "SimpleWindow" menu
     NSMenuItem *windowMenuItem = [[NSMenuItem alloc] initWithTitle:@"SimpleWindow" action:nil keyEquivalent:@""];
     NSMenu *windowMenu = [[NSMenu alloc] initWithTitle:@"Window"];
     [windowMenuItem setSubmenu:windowMenu];
     [mainMenu addItem:windowMenuItem];
 
+    // "Quit" menu item
     self.quitMenuItem = [[NSMenuItem alloc] initWithTitle:@"Quit SimpleWindow" action:@selector(quitApplication:) keyEquivalent:@"q"];
     [self.quitMenuItem setKeyEquivalentModifierMask:NSEventModifierFlagCommand];
     [windowMenu addItem:self.quitMenuItem];
 
+    // "File" menu with "New File Conversion"
+    NSMenuItem *fileMenuItem = [[NSMenuItem alloc] initWithTitle:@"File" action:nil keyEquivalent:@""];
+    NSMenu *fileMenu = [[NSMenu alloc] initWithTitle:@"File"];
+    [fileMenuItem setSubmenu:fileMenu];
+    [mainMenu addItem:fileMenuItem];
+
+    // "New File Conversion" menu item
+    NSMenuItem *newFileConversionMenuItem = [[NSMenuItem alloc] initWithTitle:@"New File Conversion" action:@selector(showFileConverter:) keyEquivalent:@""];
+    [fileMenu addItem:newFileConversionMenuItem];
+
+    // Set the app's main menu
     [NSApp setMainMenu:mainMenu];
 }
 
 - (void)quitApplication:(id)sender {
     NSLog(@"Quit application triggered.");
     [NSApp terminate:nil];
+}
+
+- (IBAction)showFileConverter:(id)sender {
+    // If the file converter window controller isn't created yet, initialize it
+    if (!self.FileConverterWindowController) {
+        self.FileConverterWindowController = [[FileConverterWindowController alloc] init];
+    }
+    // Show the file converter window
+    [self.FileConverterWindowController showFileConverterWindow];
 }
 
 @end
