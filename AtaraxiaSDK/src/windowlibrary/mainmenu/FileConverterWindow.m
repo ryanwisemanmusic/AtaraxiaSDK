d74c20b4846727dccd346446c42d0a33f0ede7e9
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-24 14:47:30 -0800

    AtaraxiaSDK V0.16.2
    Window initializes now. I forgot the initialization logic. Well, at least my comment was reflective of what needed to happen.
    
    Now, we have the start to the newest window, Currently, we are far from integrating Obj-C++ with Obj-C.

--- AtaraxiaSDK/src/windowlibrary/mainmenu/FileConverterWindow.m
+++ AtaraxiaSDK/src/windowlibrary/mainmenu/FileConverterWindow.m
@@ -3,10 +3,18 @@
 @implementation FileConverterWindow
 
 - (instancetype)init {
-    self = [super initWithWindowNibName:@"FileConverterWindow"];
+    self = [super init];
     if (self) {
-        // Initialization zone
-    }
+        NSRect frame = NSMakeRect(0, 0, 600, 400); // Adjust size as needed
+        NSWindow *window = [[NSWindow alloc] initWithContentRect:frame
+                                                      styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable)
+                                                        backing:NSBackingStoreBuffered
+                                                          defer:NO];
+        [window setTitle:@"File Converter"];
+        [self setWindow:window];
+}
+return self;
+
     return self;
 }
 
