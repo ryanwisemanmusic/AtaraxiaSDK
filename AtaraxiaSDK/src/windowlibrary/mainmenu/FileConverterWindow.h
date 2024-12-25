1482f1c9b3d9d6d375a794432c10d1b97cc98052
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-24 18:04:12 -0800

    AtaraxiaSDK V0.16.3
    We have introduced the ability for .txt .pdf, and .jpg files to be supported and loaded. There is a lot of the interface that needs designing, for example, a button that has a drop down menu for conversion type,
    
    This is going incredibly smooth, which means I'll be able to really take this aspect of the SDK to a whole new level

--- AtaraxiaSDK/src/windowlibrary/mainmenu/FileConverterWindow.h
+++ AtaraxiaSDK/src/windowlibrary/mainmenu/FileConverterWindow.h
@@ -3,6 +3,8 @@
 @interface FileConverterWindow : NSWindowController
 
 - (void)showFileConverterWindow; // Add this method to match the delegate call
+- (void)openFile:(id)sender; // Declare the openFile method
 
 @end
+
 
--- AtaraxiaSDK/src/windowlibrary/mainmenu/FileConverterWindow.m
+++ AtaraxiaSDK/src/windowlibrary/mainmenu/FileConverterWindow.m
@@ -1,26 +1,53 @@
 #import "FileConverterWindow.h"
+#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h> // Import the UniformTypeIdentifiers framework
 
 @implementation FileConverterWindow
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-        NSRect frame = NSMakeRect(0, 0, 600, 400); // Adjust size as needed
+        NSRect frame = NSMakeRect(0, 0, 600, 400); 
         NSWindow *window = [[NSWindow alloc] initWithContentRect:frame
                                                       styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable)
                                                         backing:NSBackingStoreBuffered
                                                           defer:NO];
         [window setTitle:@"File Converter"];
         [self setWindow:window];
-}
-return self;
-
+        
+        NSButton *openButton = [[NSButton alloc] initWithFrame:NSMakeRect(20, 20, 100, 30)];
+        [openButton setTitle:@"Open File"];
+        [openButton setTarget:self];
+        [openButton setAction:@selector(openFile:)];
+        [window.contentView addSubview:openButton]; 
+    }
     return self;
 }
 
 - (void)showFileConverterWindow {
-    [self showWindow:nil]; // Calls the inherited method to show the window
+    [self showWindow:nil]; 
+}
+
+- (void)openFile:(id)sender {
+    NSLog(@"Open File method called");
+
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    
+    panel.allowedContentTypes = @[
+        [UTType typeWithIdentifier:@"public.plain-text"], // for .txt files
+        [UTType typeWithIdentifier:@"com.adobe.pdf"],    // for .pdf files
+        [UTType typeWithIdentifier:@"public.jpeg"]       // for .jpg files
+    ]; 
+
+    panel.allowsMultipleSelection = NO;
+
+    if ([panel runModal] == NSModalResponseOK) {
+        NSURL *selectedFileURL = [panel URL];
+        NSLog(@"File selected: %@", selectedFileURL);
+    }
 }
 
 @end
+
+
+
 
