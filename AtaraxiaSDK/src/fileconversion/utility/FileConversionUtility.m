0b680eb756546c379cdd044ad76c43cf87de217f
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-12-29 16:31:41 -0800

    AtaraxiaSDK V0.16.5
    We have now worked with merging the file converter logic with the windowed converter. There still is an issue with the overall files converting, but I am getting separate error messages about invalid file times, so I may have to write more conversion types just so there is a back and forth between both files.

--- AtaraxiaSDK/src/fileconversion/utility/FileConversionUtility.m
+++ AtaraxiaSDK/src/fileconversion/utility/FileConversionUtility.m
@@ -1,39 +1,53 @@
 #import "FileConversionUtility.h"
 #import <Quartz/Quartz.h>
+#import <AppKit/AppKit.h>
 
 @implementation FileConversionUtility
 
 - (NSData *)convertDocumentToPDF:(NSURL *)fileURL {
-
-    NSString *documentContent = [NSString stringWithContentsofURL:fileURL encoding:NSUTF8StringEncoding error:nil];
+    NSError *error = nil;
+    NSString *documentContent = [NSString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:&error];
 
     if (!documentContent) {
-        NSLog(@"Error reading file at %@", fileURL);
+        NSLog(@"Error reading file at %@: %@", fileURL, error.localizedDescription);
         return nil;
     }
 
-    NSMutableData *pdfData = [NSMutableData data];
-    CGContextRef context = CGPDFContextCreateWithURL((__bridge CFURLRef)fileURL, NULL, NULL);
+    NSURL *tempPDFURL = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:@"temp.pdf"]];
+    CGRect mediaBox = CGRectMake(0, 0, 612, 792); // Letter size
+    CGContextRef context = CGPDFContextCreateWithURL((__bridge CFURLRef)tempPDFURL, &mediaBox, NULL);
+
+    if (!context) {
+        NSLog(@"Error creating PDF context");
+        return nil;
+    }
+
     CGContextBeginPage(context, NULL);
 
-    [documentContent drawInRect:CGRectMake(100, 100, 400, 800)
-                 withAttributes:@{NSFontAttributeName: [UIFont systemFontOfSize:12]}];
+    NSDictionary *attributes = @{NSFontAttributeName: [NSFont systemFontOfSize:12]};
+    [documentContent drawInRect:CGRectMake(100, 100, 400, 800) withAttributes:attributes];
 
     CGContextEndPage(context);
     CGContextRelease(context);
 
+    NSData *pdfData = [NSData dataWithContentsOfURL:tempPDFURL];
+    [[NSFileManager defaultManager] removeItemAtURL:tempPDFURL error:nil]; 
+
     return pdfData;
 }
 
+
 - (NSData *)convertDocumentToTXT:(NSURL *)fileURL {
-    NSString *txtContent = [NSString stringWithContentsofURL:fileURL encoding:NSUTF8StringEncoding error:nil];
+    NSError *error = nil;
+    NSString *txtContent = [NSString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:&error];
+
     if (!txtContent) {
-        NSLog(@"Error reading file at %@", fileURL);
+        NSLog(@"Error reading file at %@: %@", fileURL, error.localizedDescription);
         return nil;
     }
 
     return [txtContent dataUsingEncoding:NSUTF8StringEncoding];
 }
 
-
+@end
 
