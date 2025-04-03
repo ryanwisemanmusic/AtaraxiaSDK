dae3c71e1bea816885746b41fa3e9b8839ccb9f0
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-04-02 23:54:49 -0700

    AtaraxiaInput.hpp
    Made this read better, and also made sure to include some of the other keys that demonstrate the power of my SDK.
    
    This is a mess for now, as there are already like 3 different projects stacked into this. We will be segmenting out relevant code for these projects later, I just am too busy to properly handle this

--- src/cpp/AtaraxiaHeaders/AtaraxiaInput.hpp
+++ src/cpp/AtaraxiaHeaders/AtaraxiaInput.hpp
@@ -15,10 +15,34 @@ namespace input {
     }
     
     //This binds traditional WASD and arrow keys
-    inline bool up() { return is_key_pressed(SDL_SCANCODE_W) || is_key_pressed(SDL_SCANCODE_UP); }
-    inline bool down() { return is_key_pressed(SDL_SCANCODE_S) || is_key_pressed(SDL_SCANCODE_DOWN); }
-    inline bool left() { return is_key_pressed(SDL_SCANCODE_A) || is_key_pressed(SDL_SCANCODE_LEFT); }
-    inline bool right() { return is_key_pressed(SDL_SCANCODE_D) || is_key_pressed(SDL_SCANCODE_RIGHT); }
+    inline bool up() 
+    { 
+        return is_key_pressed(SDL_SCANCODE_W) || 
+        is_key_pressed(SDL_SCANCODE_UP); 
+    }
+    inline bool down() 
+    { 
+        return is_key_pressed(SDL_SCANCODE_S) || 
+        is_key_pressed(SDL_SCANCODE_DOWN); 
+    }
+    inline bool left() 
+    { 
+        return is_key_pressed(SDL_SCANCODE_A) || 
+        is_key_pressed(SDL_SCANCODE_LEFT); 
+    }
+    inline bool right() 
+    { 
+        return is_key_pressed(SDL_SCANCODE_D) || 
+        is_key_pressed(SDL_SCANCODE_RIGHT); 
+    }
+    inline bool n() 
+    { 
+        return is_key_pressed(SDL_SCANCODE_N); 
+    }
+    inline bool c() 
+    { 
+        return is_key_pressed(SDL_SCANCODE_C); 
+    }
 }
 
 #endif
